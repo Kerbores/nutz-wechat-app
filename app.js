7be13805cf0e27e2643d7bd651603638ea696cf7
王贵源 <guiyuan.wang-N@msxf.com>
2017-01-13 15:39:21 +0800

    图片缩小点儿

--- app.js
+++ app.js
@@ -2,9 +2,7 @@
 App({
   onLaunch: function () {
     //调用API从本地缓存中获取数据
-    var logs = wx.getStorageSync('logs') || []
-    logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
+    this.getUserInfo();
   },
   getUserInfo:function(cb){
     var that = this
@@ -16,6 +14,7 @@ App({
         success: function () {
           wx.getUserInfo({
             success: function (res) {
+              console.log(res);
               that.globalData.userInfo = res.userInfo
               typeof cb == "function" && cb(that.globalData.userInfo)
             }
